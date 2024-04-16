4d50dfcea3521981d227fa66912b6890d0f13cee
AhmedHassanKassem <121136235+AhmedHassanKassem@users.noreply.github.com>
2024-04-16 03:58:09 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -9,16 +9,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './App/Components/PageGuard/AuthContext';
 
 
-const rootElement = document.getElementById('root');
-
-ReactDOM.createRoot(rootElement).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
