783452bef1fc95d14d46c1724b7d2229361bf58c
AhmedHassanKassem <121136235+AhmedHassanKassem@users.noreply.github.com>
2024-04-16 04:48:41 +0200

    Updates

--- src/App/Components/Project-Redux/store.js
+++
@@ -1,8 +0,0 @@
-import { composeWithDevTools } from "redux-devtools-extension";
-import { applyMiddleware, legacy_createStore as createStore } from "redux";
-import appReducers from "./allReducers";
-import {thunk} from "redux-thunk";
-
-const store =  createStore(appReducers , composeWithDevTools(applyMiddleware(thunk)))
-
-export default store;
